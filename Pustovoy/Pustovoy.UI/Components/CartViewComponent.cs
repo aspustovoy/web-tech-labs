29cd1a60d0b40d5ce605bd0889a6a564dd71750e
Alexander Pustovoy <aspustovoy@gmail.com>
2024-06-20 08:55:29 +0300

    lb2

---
+++ Pustovoy/Pustovoy.UI/Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pustovoy.UI.Components
+{
+	public class CartViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
--- Pustovoy/Pustovoy.UI/Controllers/HomeController.cs
+++ Pustovoy/Pustovoy.UI/Controllers/HomeController.cs
@@ -1,18 +1,39 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Pustovoy.UI.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+		private readonly ILogger<HomeController> _logger;
+		private readonly List<ListDemo> _listData;
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+			_listData = new List<ListDemo>
+            {
+            new ListDemo {Id=1, Name="Item 1"},
+            new ListDemo {Id=2, Name="Item 2"},
+            new ListDemo {Id=3, Name="Item 3"}
+            };
+		}
+		// GET: Home
+		public ActionResult Index()
         {
-            return View();
-        }
+			ViewData["text"] = "Лабараторная работа №2";
+			SelectList data = new SelectList(_listData, "Id", "Name");
+			return View(data);
+		}
 
-        // GET: Home/Details/5
-        public ActionResult Details(int id)
+		public class ListDemo
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		// GET: Home/Details/5
+		public ActionResult Details(int id)
         {
             return View();
         }
